5143be6e5a4604089af7eabb5dd26a0ce7d9d356
sairlseb <cyrilseberiaga@gmail.com>
2023-09-11 11:47:34 +0800

    added badge to pending and received

--- resources/views/incoming/index.blade.php
+++ resources/views/incoming/index.blade.php
@@ -20,7 +20,7 @@
                         <tr>
                             <td>{{ $incoming->files[0]->filename }}</td>
                             <td>{{ $incoming->files[0]->user->username }}</td>
-                            <td>{{ $incoming->status == 0 ? 'Pending' : 'Received' }}</td>
+                            <td><p class="badge {{ $incoming->status == 0 ? 'badge-warning' : 'badge-success' }} text-wrap">{{ $incoming->status == 0 ? 'Pending' : 'Received' }}</p></td>
                             <td>
                                 <button type="button" class="btn btn-primary btn-sm">Received</button>
                                 <button type="button" class="btn btn-secondary btn-sm">Download</button>
